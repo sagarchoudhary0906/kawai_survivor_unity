aa33480ca3c370e49021c3a412ea51413380f7fe
Sagar Choudhary <sagarchoudhary15947@gmail.com>
2025-07-25 06:51:10 +0530

    pushing nitial setup of player in main camera view and adding player controller script in it's initial phase

---
+++ Assets/Kawaii Survivor/Fonts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rig;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        rig.linearVelocity = Vector2.right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
