dffe32ab56097e695e91d890ad837acb99edf3cb
Sagar Choudhary <sagarchoudhary15947@gmail.com>
2025-07-25 08:05:29 +0530

    clamping the camera with player/dave movement

--- Assets/Kawaii Survivor/Fonts/PlayerController.cs
+++ Assets/Kawaii Survivor/Fonts/PlayerController.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
-{
+{   
     private Rigidbody2D rig;
+    [Header("Elements")]
     [SerializeField]private MobileJoystick playerJoystick;
+    
+    [Header("Settings")]
     [SerializeField] private float moveSpeed;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
--- Assets/Mobile Joystick/Scripts/CameraController.cs
+++ Assets/Mobile Joystick/Scripts/CameraController.cs
@@ -3,12 +3,28 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [Header("Elements")]
     [SerializeField] private Transform target;
+    
+    [Header("Settings")]
+    [SerializeField] private Vector2 minMaxXY;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void LateUpdate()
     {
+        if (!target)
+        {
+            return;
+        }
         Vector3 targetPosition = target.position;
         targetPosition.z = -10;
+        
+        // Calmping camer taget posiiton with minX maxX and minY and maxY
+        
+        targetPosition.x = Mathf.Clamp(targetPosition.x, -minMaxXY.x, minMaxXY.x);
+        targetPosition.y = Mathf.Clamp(targetPosition.x, -minMaxXY.y, minMaxXY.y);
+        
         transform.position = targetPosition;
     }
 }
