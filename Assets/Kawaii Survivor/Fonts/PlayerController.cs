b2522a2ce74a9c33c72ea324497a0fbc93574483
Sagar Choudhary <sagarchoudhary15947@gmail.com>
2025-07-25 07:05:01 +0530

    adding control of player movement with the the help of joystick

--- Assets/Kawaii Survivor/Fonts/PlayerController.cs
+++ Assets/Kawaii Survivor/Fonts/PlayerController.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rig;
+    [SerializeField]private MobileJoystick playerJoystick;
+    [SerializeField] private float moveSpeed;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         rig.linearVelocity = Vector2.right;
+        moveSpeed = 7;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rig.linearVelocity = playerJoystick.GetMoveVector() * moveSpeed * Time.deltaTime;
     }
 }
